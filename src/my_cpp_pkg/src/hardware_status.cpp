2fa3159abe82e51fddd7ca7d7c81af1bc8f2d691
maanto <info.martinanto@gmail.com>
2025-04-11 20:33:37 +0200

    added new changes to learn github

--- src/my_cpp_pkg/src/hardware_status.cpp
+++ src/my_cpp_pkg/src/hardware_status.cpp
@@ -34,7 +34,7 @@ int main(int argc, char * argv[])
 
     auto node = std::make_shared<HardwareStatusNode>();
     RCLCPP_INFO(node->get_logger(), "Hello World! from node2");
-    // testing git commit again
+    // testing git commit again..
 
     rclcpp::spin(node);
     rclcpp::shutdown();
